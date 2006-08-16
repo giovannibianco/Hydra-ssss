730546db80c6c6e3717603d986e86db271861fc0
taspelie <taspelie@4c42d608-eb89-46b6-9e62-40a981e1fec6>
2006-08-16 16:00:44 +0000

    Script for test of ssss splitting and joining for ascii

---
+++ src/test-shamir-ascii.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#export GLITE_SSSS_LOG_LEVEL=DEBUG
+
+# exit after the first problem
+set -e
+
+#KEY32=$(./glite-ssss-generate-key 32)
+# Need a not hex test. overriding:
+KEY32="abcdefghijklmopqrstuvwxyz#¤&/()"
+./test-shamir-ascii 5 2  $KEY32
+
+KEY8=$(./glite-ssss-generate-key 8)
+./test-shamir-ascii 7 3 $KEY8
+
+echo ""
+echo "Testing glite-ssss-split-passwd and glite-ssss-join-passwd"
+cmd="./glite-ssss-split-passwd -q 5 2 $KEY32"
+echo $cmd
+SPLIT32=$($cmd)
+cmd="./glite-ssss-join-passwd -q $SPLIT32"
+echo $cmd
+JOIN32=$($cmd)
+echo "Joined shares: $JOIN32"
+if [ "$KEY32" != "$JOIN32" ]; then
+    echo "Error: split-join failed!"
+    exit -1
+fi
+
