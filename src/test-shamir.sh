995c97d589983c5d32df95805a209a42d0529c05
szamsu <szamsu@4c42d608-eb89-46b6-9e62-40a981e1fec6>
2006-08-16 09:32:31 +0000

    testing the CLI tools

--- src/test-shamir.sh
+++ src/test-shamir.sh
@@ -11,3 +11,17 @@ KEY32=$(./glite-ssss-generate-key 32)
 KEY8=$(./glite-ssss-generate-key 8)
 ./test-shamir 7 3 $KEY8
 
+echo ""
+echo "Testing glite-ssss-split-key and glite-ssss-join-key"
+cmd="./glite-ssss-split-key -q 5 2 $KEY32"
+echo $cmd
+SPLIT32=$($cmd)
+cmd="./glite-ssss-join-key -q $SPLIT32"
+echo $cmd
+JOIN32=$($cmd)
+echo "Joined shares: $JOIN32"
+if [ "$KEY32" != "$JOIN32" ]; then
+    echo "Error: split-join failed!"
+    exit -1
+fi
+
